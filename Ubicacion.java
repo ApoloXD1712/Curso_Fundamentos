1477fca1206222ac55053915ab6b107c8f99d573
ApoloXD1712 <104176492+ApoloXD1712@users.noreply.github.com>
2022-04-27 23:13:13 -0500

    Update Ubicacion.java

--- Ubicacion.java
+++ Ubicacion.java
@@ -1,14 +1,16 @@
-public class Ubicacion {
+import java.util.*;
+
+public class Ubicacion implements Comparable<Ubicacion> {
     private String nombree;
     private float distancia;
     private int cantidad;
+    public static List<Ubicacion> ubicaciones = new ArrayList<>();
 
     public Ubicacion(String no, float di, int ca) {
         this.nombree = no;
         this.distancia = di;
         this.cantidad = ca;
     }
-
 
     public String getNombre() {
         return nombree;
@@ -34,14 +36,47 @@ public class Ubicacion {
         this.cantidad = cantidad;
     }
 
-    public Ubicacion(){
+    public int compareTo(Ubicacion e) {
+        if (e.getDistancia() > distancia) {
+            return -1;
+        } else if (e.getDistancia() == distancia) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
+
+    public static String casoNueve() {
+        String contador = "";
+        int posicionMenor = 0;
+
+        for (int i = 0; i < Ubicacion.ubicaciones.size(); i++) {
+            Ubicacion p = Ubicacion.ubicaciones.get(1);
+            if(i==0){
+                posicionMenor =  p.getCantidad();
+                contador = "El lugar es "+ p.getNombre() + " la distancia es de "+ p.getDistancia() + " con una invasion de "+ p.getCantidad() + " zombies";
+            }
+            else{
+                if(p.getCantidad()<posicionMenor){
+                    posicionMenor = p.getCantidad();
+                    contador= "El lugar es " + p.getNombre() + " la distancia es de " + p.getDistancia() +" con una invasion de " + p.getCantidad() + " zombies";
+
+                }
+            }
+        }
+        return contador;
+
+    }
+
+    public Ubicacion() {
+    }
+
     @Override
     public String toString() {
         return "Ubicacion{" +
-                "nombree='" + nombree + '\'' +
-                ", distancia=" + distancia +" "+"m"+
-                ", cantidad=" + cantidad +
+                "nombree:'" + nombree + '\'' +
+                ", distancia:" + distancia + " " + "m" +
+                ", cantidad:" + cantidad +
                 '}';
     }
 }
